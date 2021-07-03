1190fbc6790a459be1d7005402c9798e742c7e07
nathan-mccraw <naterltw175@gmail.com>
2021-07-03 11:31:13 -0400

    Add axios code lines - nonfunctional

--- src/App.js
+++ src/App.js
@@ -10,40 +10,52 @@ const App = () => {
     e.preventDefault();
     setHandleQuery("");
   };
-  const [characterData, setCharacterData] = useState([
-    {
-      name: "Test Character 1",
-      birth: "01/01/01",
-      height: "100 cm",
-      mass: "100 kg",
-      home: "home1URL",
-      species: "human",
-    },
-    {
-      name: "Test Character 2",
-      birth: "02/02/02",
-      height: "200 cm",
-      mass: "200 kg",
-      home: "home2URL",
-      species: "droid",
-    },
-    {
-      name: "Test Character 3",
-      birth: "03/03/03",
-      height: "300 cm",
-      mass: "300 kg",
-      home: "home3URL",
-      species: "clone",
-    },
-    {
-      name: "Test Character 4",
-      birth: "04/04/04",
-      height: "400 cm",
-      mass: "400 kg",
-      home: "home4URL",
-      species: "beast",
-    },
-  ]);
+  const [characterData, setCharacterData] = useState(APIreturnObject.results);
+
+  //         [
+  //     {
+  //       name: "Test Character 1",
+  //       birth: "01/01/01",
+  //       height: "100 cm",
+  //       mass: "100 kg",
+  //       home: "home1URL",
+  //       species: "human",
+  //     },
+  //     {
+  //       name: "Test Character 2",
+  //       birth: "02/02/02",
+  //       height: "200 cm",
+  //       mass: "200 kg",
+  //       home: "home2URL",
+  //       species: "droid",
+  //     },
+  //     {
+  //       name: "Test Character 3",
+  //       birth: "03/03/03",
+  //       height: "300 cm",
+  //       mass: "300 kg",
+  //       home: "home3URL",
+  //       species: "clone",
+  //     },
+  //     {
+  //       name: "Test Character 4",
+  //       birth: "04/04/04",
+  //       height: "400 cm",
+  //       mass: "400 kg",
+  //       home: "home4URL",
+  //       species: "beast",
+  //     },
+  //   ]);
+
+  const QueryAPI = async (query) => {
+    try {
+      return axios.get(`"https://swapi.dev/api/people/?search="${query}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const APIreturnObject = await QueryAPI("");
 
   return (
     <div>
